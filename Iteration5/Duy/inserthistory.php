285f1a7cd37909c0d9238084e2af023254b0b422
Duy Do <duy.ado@csu.fullerton.edu>
2019-05-08 13:03:07 -0700

    Update

--- Iteration5/Duy/inserthistory.php
+++ Iteration5/Duy/inserthistory.php
@@ -1,8 +1,10 @@
 <?php
+//This is used to insert the user swipe right option into history
 require "conn.php";
 $restaurant_name = isset($_POST['rName']) ? $_POST['rName'] : '';
 $user_id = isset($_POST['userID']) ? $_POST['userID'] : '';
 $place_id = isset($_POST['placeID']) ? $_POST['placeID'] : '';
+photoref = isset($_POST['photoref']) ? $_POST['photoref'] : '';
 $mysql_query = "SELECT * FROM history_data3 WHERE rname = '$restaurant_name' AND  placeID ='$place_id' AND userID='$user_id'";
 $conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
 $result=mysqli_query($conn, $mysql_query);
@@ -10,8 +12,8 @@ if (mysqli_num_rows($result)>0) {
 	echo "Already Exist";
 }
 else{
-    $mysql_query = "INSERT INTO history_data3 (rname, placeID, userID)
-                VALUES ('$restaurant_name','$place_id','$user_id')";
+    $mysql_query = "INSERT INTO history_data3 (rname, placeID, userID,photoref)
+                VALUES ('$restaurant_name','$place_id','$user_id','$photoref')";
     $conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
     if ($conn->query($mysql_query) === TRUE) {
         echo "Insert History Successed";
