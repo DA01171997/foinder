f8797483de2c73e75f4f3a1d1ed27f5dfa78e58d
Duy Do <duy.ado@csu.fullerton.edu>
2019-05-08 13:00:32 -0700

    Update gethistory.php

--- Iteration5/Duy/gethistory.php
+++ Iteration5/Duy/gethistory.php
@@ -9,6 +9,10 @@ if ($option=="placeID"){
 	$rname = isset($_POST['rname']) ? $_POST['rname'] : '';
 	$mysql_query = "SELECT $option FROM history_data3 WHERE userID='$user_id' AND rname ='$rname' ";
 }
+if ($option=="photoref"){
+	$rname = isset($_POST['rname']) ? $_POST['rname'] : '';
+	$mysql_query = "SELECT $option FROM history_data3 WHERE userID='$user_id' AND rname ='$rname' ";
+}
 //echo $mysql_query;
 $conn = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
 $result=mysqli_query($conn, $mysql_query);
@@ -24,6 +28,11 @@ if ($num>0) {
 	if ($option=="placeID"){
 		$assoarray = mysqli_fetch_assoc($result);
     		$matchedResult = $assoarray['placeID'];
+		echo $matchedResult;
+	}
+	if ($option=="photoref"){
+		$assoarray = mysqli_fetch_assoc($result);
+    		$matchedResult = $assoarray['photoref'];
 		echo $matchedResult;
 	}
 
